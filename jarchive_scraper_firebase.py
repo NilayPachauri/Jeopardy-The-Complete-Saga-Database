6c214e9a04933751ec66c173293cce315373fbbe
nilaypachauri <pachauri@usc.edu>
2020-11-13 01:16:09 -0800

    Updated firebase script to batch write and account for other errors found in the dynamodb script

--- jarchive_scraper_firebase.py
+++ jarchive_scraper_firebase.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup
 import scraperwiki
+
 from datetime import datetime
+import pickle
 import re
+import sys
 import unicodedata
 
 import firebase_admin
@@ -16,25 +19,21 @@ cred = credentials.Certificate('./firebase_key.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
+# Load the already processed episodes
+processed = pickle.load(open('./firebase_processed.p', 'rb'))
+
 def scrape_all_seasons(url):
 
     soup = BeautifulSoup(scraperwiki.scrape(url), features='lxml')
 
-    # list of already scraped seasons
-    scraped_seasons = ['Season 37', 'Jeopardy!: The Greatest of All Time', 'Season 36']
-    
     #Grab all of the seasons listed
     seasons = soup.find('div', {"id":"content"}).findAll('a')
-    count = 0
     for season in seasons:
+
         season_name = unicodedata.normalize('NFKC', season.text)
 
         print('Scraping ' + season_name + ' from ' + season['href'])
-
-        # Only Scrape the Season if it has not been scraped
-        if season_name not in scraped_seasons:
-            scrape_season(base_url+season['href'], season_name)
-
+        scrape_season(base_url+season['href'], season_name)
         print('Finished scraping ' + season_name)
         print()
 
@@ -45,21 +44,25 @@ def scrape_season(url, season):
     #Grab the div that contains the content and search for any links
     episodes = soup.find('div', {"id":"content"}).findAll('a',{"href":re.compile('showgame\.php')})
     for episode in episodes:
-        ep_data = unicodedata.normalize('NFKC', episode.text).split(',')
-        ep_num = int(re.search('#(\d+)', ep_data[0]).group(1))
+        try:
+            ep_data = unicodedata.normalize('NFKC', episode.text).split(',')
+            ep_num = int(re.search('#(\d+)', ep_data[0]).group(1))
 
-        # Get the Date
-        air_data = re.search('(\d{4}-\d{2}-\d{2})', ep_data[1]).group(1) + ' UTC'
-        air_datetime = datetime.strptime(air_data, '%Y-%m-%d %Z')
-        air_date = air_datetime.strftime('%Y/%m/%d')
+            # Get the Date
+            air_data = re.search('(\d{4}-\d{2}-\d{2})', ep_data[1]).group(1) + ' UTC'
+            air_datetime = datetime.strptime(air_data, '%Y-%m-%d %Z')
+            air_date = air_datetime.strftime('%Y/%m/%d')
 
-        print('\tScraping Episode {} from {}'.format(ep_num, episode['href']))
-        scrape_episode(episode['href'], season, ep_num, air_date)
+            print('\tScraping Episode {} from {}'.format(ep_num, episode['href']))
+            scrape_episode(episode['href'], season, ep_num, air_date)
+        except Exception as e:
+            print('\tCould not correctly parse ' + unicodedata.normalize('NFKC', episode.text).strip())
+        sys.stdout.flush()
 
 
 def scrape_episode(url, season, episode, air_date):
-    # Warm Start Due to Google Firebase Quota Limits
-    if episode > 7937:
+    # Warm Start Due to Errors
+    if season in processed and episode in processed[season]:
         return
     
     try:
@@ -88,30 +91,42 @@ def scrape_episode(url, season, episode, air_date):
             if final_jeopardy_round_exists:
                 categories['FJ'] = [(cats[12] if double_jeopardy_round_exists else cats[6]) if jeopardy_round_exists else cats[1]]
             
-            allClues = soup.findAll(attrs={"class" : "clue"})
-            for clue in allClues:
+            # Perform a Batch Write for all Clues in an Episode
+            with table.batch_writer() as batch:
+                allClues = soup.findAll(attrs={"class" : "clue"})
+                for clue in allClues:
 
-                # The Final Jeopardy Div is not located in the same place
-                # as other questions so it must be found seperately
-                fj_div = None
-                if not clue.find('div') and clue.find(id='clue_FJ'):
-                    fj_div = clue.parent.parent.find('div')
+                    # The Final Jeopardy Div is not located in the same place
+                    # as other questions so it must be found seperately
+                    fj_div = None
+                    if not clue.find('div') and clue.find(id='clue_FJ'):
+                        fj_div = clue.parent.parent.find('div')
 
-                clue_attribs = get_clue_attribs(clue, categories, fj_div)
-                if clue_attribs:
-                    clue_attribs['air_date'] = air_date
-                    clue_attribs['season'] = season
-                    clue_attribs['episode'] = episode
-        
-                    #a shitty unique id but it should do
-                    uid = ': '.join([str(episode), clue_attribs['category'], str(clue_attribs['dollar_value'])])
+                    clue_attribs = get_clue_attribs(clue, categories, fj_div)
+                    if clue_attribs:
+                        clue_attribs['air_date'] = air_date
+                        clue_attribs['season'] = season
+                        clue_attribs['episode'] = episode
+            
+                        # Create Unique Identification
+                        uid = ': '.join([season, str(episode), clue_attribs['category'], str(clue_attribs['dollar_value'])])
 
-                    # Replace potential forward slashes with backward slashes
-                    # Note: Firebase forward slash represeents a new collection
-                    uid = uid.replace('/', '\\')
+                        # Replace potential forward slashes with backward slashes
+                        # Note: Firebase forward slash represeents a new collection
+                        uid = uid.replace('/', '\\')
 
-                    doc_ref = db.collection(u'clues').document(uid)
-                    doc_ref.set(clue_attribs)
+                        doc_ref = db.collection(u'clues').document(uid)
+                        doc_ref.set(clue_attribs)
+
+            # Update the Processed Dictionary with the most recently processed episode
+            if season in processed:
+                processed[season].append(episode)
+            else:
+                processed[season] = [episode]
+
+            # Write out the pickle file
+            with open('./dynamodb_processed.p', 'wb') as file:
+                pickle.dump(processed, file)
 
     except RuntimeError:
         exception = 1
@@ -135,16 +150,7 @@ def get_clue_attribs(clue, cats, fj_div=None):
         cat_order = 1 if j_type == 'FJ' else int(clue_props[3])
 
         #Now to figure out the category
-        # if j_type == "FJ":
-        #     cat = cats[12]
-        # elif j_type == "DJ":
-        #     cat = cats[cat_num+5]
-        # else:
-        #     cat = cats[cat_num-1]
         cat = cats[j_type][cat_num - 1]
-
-        #Are we in double jeopardy?
-        dj = clue_props[1] == "DJ"
 
         #The class name for the dollar value varies if it's a daily double
         dollar_value = "FJ: $0" if j_type == 'FJ' else clue.find(attrs={"class" : re.compile('clue_value*')}).text
